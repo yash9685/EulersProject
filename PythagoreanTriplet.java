323dcbfd23e5e2527bea806404b35f98051acd74
yash9685 <45978218+yash9685@users.noreply.github.com>
2019-01-11 15:33:36 +0530

    Add files via upload

---
+++ PythagoreanTriplet.java
@@ -0,0 +1,23 @@
+
+public class PythagoreanTriplet {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		for(int i=1;i<1000;i++) {
+			a=i;
+			for(int j=1;j<1000;j++) {
+				b=j;
+				for(int k=1;k<1000;k++) {
+					c=k;
+					if(((a+b+c)==1000)&&(a<b&&b<c)&&((a*a+b*b)==c*c)) {
+						System.out.println("a= "+a+" b= "+b+" c= "+c);
+						int product=a*b*c;
+						System.out.println("The product is = "+product);
+					}
+				}
+			}
+		}
+
+	}
+
+}
