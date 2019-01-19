503a7fb233a344149b126d7ece8b716dee624330
yash9685 <45978218+yash9685@users.noreply.github.com>
2019-01-19 16:01:58 +0530

    Add files via upload

---
+++ Euler10PrimeSum.java
@@ -0,0 +1,28 @@
+
+public class Euler10PrimeSum {
+
+	public static void main(String[] args) {
+	     int  MAX = 2000000;
+	     long sum = 0;
+		    for (int i = 2; i < MAX; i++){
+		        if (checkPrime(i) == 1){
+		            sum += i;
+		        }
+		    } System.out.println(sum);
+	}
+      int MAX = 2000000;
+	public static int checkPrime(int n){
+	    int range = n;
+	    for (int i = 2; i < range; i++){
+	        if (n%i == 0){
+	            return 0;
+	        }
+	        range = n / i;
+	    }
+	    return 1;
+	}
+
+	
+	}
+
+
